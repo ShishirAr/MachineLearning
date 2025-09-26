6a9e051a1f625a57881ba8c136357ec258cfe503
ShishirAr <aryalshishir79@gmail.com>
2025-09-26 01:33:10 -0500

    new file

--- index.py
+++ index.py
@@ -1,12 +1,37 @@
 print(" this is me creating new repo!!")
 
 
-""" create new repo
+"""
+ create brand new repo
+ (first create new repo from github.com)
+
     git init
     git add .
     git commit -m "text"
+    git branch -M main
+    git remote add origin https://github.com/username/reop_name.git
     git push -u origin main
 
 
 
  """
+
+
+
+""" to add new file to current project
+
+    git add index4.py
+    git commit -m "Text"
+    git push
+
+"""
+
+
+'''
+
+just commit changes in already existing file in repo
+
+git commit -am "text"
+git push
+
+'''
